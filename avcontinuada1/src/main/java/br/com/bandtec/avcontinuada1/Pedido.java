400324da7a9e74135f00209770f25885b71c27d2
anna-guercio <anna.guercio@bandtec.com.br>
2021-03-03 00:42:37 -0300

    colocando a justificativa

--- avcontinuada1/src/main/java/br/com/bandtec/avcontinuada1/Pedido.java
+++ avcontinuada1/src/main/java/br/com/bandtec/avcontinuada1/Pedido.java
@@ -2,8 +2,10 @@ package br.com.bandtec.avcontinuada1;
 
 public abstract class Pedido implements Tributavel {
 
-    // Justificativa:
-    // Utilizei classe abstrata porque queria que meu projeto tivesse uma relação de herança
+    /* Justificativa:
+    * Utilizei classe abstrata porque queria que meu projeto tivesse uma relação de herança das classes que seriam
+    * herdeiras dela, no caso, as classes Lanche e Bebida que extendem a classe abstrata Pedido.
+    */
 
     // Atributos
     private Integer numero; // número do pedido
--- avcontinuada1/src/main/java/br/com/bandtec/avcontinuada1/Tributavel.java
+++ avcontinuada1/src/main/java/br/com/bandtec/avcontinuada1/Tributavel.java
@@ -5,6 +5,10 @@ public interface Tributavel {
     // Método implicitamente abstrato
     public Double getValorTributo();
 
-    // Justificativa:
+    /* Justificativa:
+    * Utilizei a interface Tributável para que seria possível eu obrigar as classes que implementam a interface
+    * a implementar um método abstrato, de forma que não exista relação de herança nestas classes e não possua atributos.
+    * Neste caso, o método força a classe a implementar ele e cobrar um valor de tributo.
+    */
 
 }
